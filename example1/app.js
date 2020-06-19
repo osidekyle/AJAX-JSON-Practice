d265946ed80a89400801cf05a7a852c0249afa5e
osidekyle <hawkinskyle67@gmail.com>
2020-06-18 21:55:35 -0700

    example JSON

---
+++ example1/app.js
@@ -0,0 +1,16 @@
+var thePets=[
+    {
+    "name":"Meowsalot",
+    "species": "cat",
+    "favFood":"tuna"
+    },
+    {
+    "name":"Barkey",
+    "species": "dog",
+    "favFood":"carrots"
+    }
+]
+
+
+
+thePets[1].name
