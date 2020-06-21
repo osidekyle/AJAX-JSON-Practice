464fecb4215046e8d694df447dc52469d346e28c
osidekyle <hawkinskyle67@gmail.com>
2020-06-21 09:07:48 -0700

    added likes and dislikes, hid button afer third click

--- example1/app.js
+++ example1/app.js
@@ -16,13 +16,40 @@ button.addEventListener('click',function(){
 
     ourRequest.send();
     pageCounter++;
+    if(pageCounter>3){
+        button.style.display='none';
+    }
 });
 
 function renderHTML(data){
     var htmlString="";
 
     for(let i=0;i<data.length;i++){
-        htmlString+="<p> "+data[i].name + " is a " + data[i].species + ".<p>"
+        htmlString+="<p> "+data[i].name + " is a " + data[i].species + " that likes to eat ";
+        
+        for(let ii=0;ii<data[i].foods.likes.length;ii++){
+           if(ii==0){
+            htmlString+=data[i].foods.likes[ii];
+           }
+           else{
+            htmlString+=" and "+ data[i].foods.likes[ii];
+           }
+
+        }
+
+        htmlString+=" and dislikes ";
+        for(let ii=0;ii<data[i].foods.dislikes.length;ii++){
+            if(ii==0){
+             htmlString+=data[i].foods.dislikes[ii];
+            }
+            else{
+             htmlString+=" and "+ data[i].foods.dislikes[ii];
+            }
+ 
+         }
+
+
+        htmlString+="<p>";
     }
 
     animalContainer.insertAdjacentHTML('beforeend',htmlString)
